aa77794516aeaa7f0f80283ab7a91a4e87435bbc
Facundo Carvajal <carvajalfacundo28@gmail.com>
2025-08-06 11:52:56 -0300

    Modifique codigo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 // creo formulario
 let formulario = document.getElementById("formulario");
+
 // formulario enviado
 formulario.addEventListener("submit",function (enviado) {
     enviado.preventDefault();
     alert("Enviado correctamente");
     formulario.reset();
+});
+
+// Botón hamburguesa
+let menuhamb = document.getElementById("menu-hamb");
+const menu = document.getElementById("menu");
+
+menuhamb.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
 });
